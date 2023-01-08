5dac65b0e69efe2582ed22fcd0f45516b1d665de
misaki <stn061118@gmail.com>
2023-01-08 11:31:33 +0000

    課題

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
