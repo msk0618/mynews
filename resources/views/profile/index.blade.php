71aa738a0eeb48e29db8d4ca739af4ae891f5d53
misaki <stn061118@gmail.com>
2023-03-23 09:56:20 +0000

    課題プロフィール

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,33 +1,21 @@
 @extends('layouts.front')
-@section('title', 'マイニュースの投稿画面')
+@section('title', 'プロフィール一覧')
 
 
 @section('content')
     <div class="container">
         <hr color="#c0c0c0">
-        @if (!is_null($headline))
             <div class="row">
-                <div class="headline col-md-10 mx-auto">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ secure_asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
-                                <div class="title p-2">
-                                    <h1>{{ Str::limit($headline->title, 70) }}</h1>
-                                </div>
+                                <div class="image"></div>
+                                <div class="title p-2"></div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <p class="body mx-auto">{{ Str::limit($headline->body, 650) }}</p>
-                        </div>
+                        <div class="col-md-6"></div>
                     </div>
-                </div>
             </div>
-        @endif
         <hr color="#c0c0c0">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
@@ -41,14 +29,14 @@
                                 <div class="title">
                                     {{ Str::limit($post->name, 150) }}
                                 </div>
-                                 <div class="title">
+                                <div class="title">
                                     {{ Str::limit($post->gender, 150) }}
                                 </div>
-                                 <div class="title">
+                                <div class="title">
                                     {{ Str::limit($post->hobby, 150) }}
                                 </div>
-                                <div class="body mt-3">
-                                    {{ Str::limit($post->introduction, 300) }}
+                                <div class="title">
+                                    {{ Str::limit($post->introduction, 150) }}
                                 </div>
                             </div>
                         </div>
@@ -57,6 +45,5 @@
                 @endforeach
             </div>
         </div>
-    </div>
     </div>
 @endsection
